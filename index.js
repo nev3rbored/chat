f51125b911399aa008ee35cc998d2da86f4c3477
PoolloverNathan <nathan.kulzer@protonmail.com>
2022-10-09 16:54:23 +0000

    hey look, a submodule!

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express")
+const router = express.Router()
+router.use(express.static(__dirname + '/static'))
+
+// #region
+function addAuthView(url, view, provider, ...before) {
+  return addView(url, view, provider, ...before, requiresAuth())
+}
+/**
+ * @param {string} url
+ * @param {string} view
+ * @param {object | (req, res) => object} provider
+ * @param {((req, res) => void)[]} before
+ * @returns {void}
+ */
+function addView(url, view = url, provider = {}, ...before) {
+  app.get(typeof url === 'string' ? '/' + url : url, before, (req, res) => {
+    render(req, res, view, (typeof provider === 'function' ? provider(req, res) : provider))
+  })
+}
+function render(req, res, view, settings) {
+  res.render(`@chat/${view}.pug`/, { ...req.params, req, res, db, ...settings })
+}
+// #endregion
+
+addView('/', 'root.pug')
